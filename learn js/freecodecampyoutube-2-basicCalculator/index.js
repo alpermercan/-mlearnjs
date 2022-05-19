f304d84b05941224c8f8ee6e377030ca8cdddd41
Alper Mercan <83232150+alpermercan@users.noreply.github.com>
2022-05-19 09:10:30 +0300

    Add files via upload

---
+++ learn js/freecodecampyoutube-2-basicCalculator/index.js
@@ -0,0 +1,25 @@
+
+let num1 = document.getElementById("num-1").value
+let num2 = document.getElementById("num-2").value
+
+console.log(num1)
+
+
+
+
+function add(){
+    console.log(num1)
+    
+}
+
+function subtract(){
+    
+}
+
+function dvide(){
+    
+}
+
+function multiply(){
+    
+}
